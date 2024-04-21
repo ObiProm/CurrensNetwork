fbcac0f3b3202c7645f8adf0a72d5f8abde2b907
ObiProm <zharkovmichael@gmail.com>
2024-04-21 15:25:04 +0300

    Updated Packet class

--- code/Packet.cs
+++ code/Packet.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Net;
+using System.Net.Sockets;
 
 namespace CurrensNetwork
 {
     /// <summary>
     /// Data which CurrensNetwork uses for transpotring
     /// </summary>
-    [Serializable]
     public class Packet
     {
         /// <summary>
@@ -17,9 +17,47 @@ namespace CurrensNetwork
         /// Args of method
         /// </summary>
         public object[] Params { get; set; }
+        internal short paramsCount = 0;
         /// <summary>
         /// Id of user, which get data
         /// </summary>
         public ulong SendTo = 0;
+
+        internal byte[] Pack()
+        {
+            using MemoryStream memoryStream = new();
+            using BinaryWriter writer = new(memoryStream);
+            paramsCount = (short)Params.Length;
+
+            writer.Write(Name);
+            writer.Write(paramsCount);
+
+            foreach (object param in Params)
+                writer.Write(param.ToString());
+
+            writer.Write(SendTo);
+
+            byte[] data = memoryStream.ToArray();
+            return data;
+        }
+        internal static Packet Unpack(NetworkStream stream)
+        {
+            BinaryReader reader = new(stream);
+
+            Packet packet = new();
+            packet.Name = reader.ReadString();
+
+            List<object> paramsList = new();
+            int paramsCount = reader.ReadInt16();
+
+            for (int i = 0; i < paramsCount; i++)
+                paramsList.Add(reader.ReadString());
+
+            packet.Params = paramsList.ToArray();
+
+            packet.SendTo = reader.ReadUInt64();
+
+            return packet;
+        }
     }
 }
